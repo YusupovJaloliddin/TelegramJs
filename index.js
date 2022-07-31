53b9c0c386db7cc589b0b8718758f24cd1394323
YusupovJaloliddin <yusupovjaloliddin4633@gmail.com>
2022-07-31 09:41:26 +0500

    i create new TelegramBot

---
+++ index.js
@@ -0,0 +1,41 @@
+const { Telegraf } = require('telegraf')
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.command('quit', (ctx) => {
+  // Explicit usage
+  ctx.telegram.leaveChat(ctx.message.chat.id)
+
+  // Using context shortcut
+  ctx.leaveChat()
+})
+
+bot.on('text', (ctx) => {
+  // Explicit usage
+  ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
+
+  // Using context shortcut
+  ctx.reply(`Hello ${ctx.state.role}`)
+})
+
+bot.on('callback_query', (ctx) => {
+  // Explicit usage
+  ctx.telegram.answerCbQuery(ctx.callbackQuery.id)
+
+  // Using context shortcut
+  ctx.answerCbQuery()
+})
+
+bot.on('inline_query', (ctx) => {
+  const result = []
+  // Explicit usage
+  ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result)
+
+  // Using context shortcut
+  ctx.answerInlineQuery(result)
+})
+
+bot.launch()
