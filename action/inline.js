9aab2b7c2b25d5cdd7f42e57cd672ed853d94f73
YusupovJaloliddin <yusupovjaloliddin4633@gmail.com>
2022-07-31 18:55:05 +0500

    i added callback button

--- action/inline.js
+++ action/inline.js
@@ -5,6 +5,7 @@ bot.command('inlineSilka' , ctx => {
     const keyboard = Markup.inlineKeyboard(
         [
             Markup.urlButton('Google','google.com'),
+            Markup.urlButton('Author','t.me/uzmu4633')
         ]
     )
     ctx.telegram.sendMessage(
@@ -25,7 +26,7 @@ bot.command('inlineCallback' , ctx => {
     )
     ctx.telegram.sendMessage(
         ctx.from.id,
-        'Testing url buttons',{
+        'Quyidagi tugmalani birini tanlang',{
         reply_markup:keyboard
         }
     )
